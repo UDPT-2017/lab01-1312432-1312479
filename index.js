9ac832a730ec5a1b891ce8728d83cdc7fec0e740
1312432 <1312432@student.hcmus.edu.vn>
2017-04-10 15:57:58 +0700

    add layout

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var app = express();
+  var express = require('express');
+  var exphbs  = require('express-handlebars');
+  var app = express();
 
-app.use(express.static('public'));
-app.use('/components', express.static('bower_components'));
+  app.use(express.static('public'));
+  app.use('/components', express.static('bower_components'));
 
-app.engine('hbs', exphbs({}));
-app.set('view engine', 'hbs');
+  app.engine('hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'application'
+  }));
+  app.set('view engine', 'hbs');
 
-app.get('/hbs', function (req, res) {
-   res.render('index', { title: ' Hey HomePage', message: 'Hello there!' });
-});
+  app.get('/', function (req, res) {
+    res.render('index', {
+      title: ' Hey HomePage',
+      message: 'My HomePage',
+   });
+  });
 
-app.get('/albums', function (req, res) {
+  app.get('/albums', function (req, res) {
     var images = [
       'image/image1.jpg',
       'image/image2.jpg',
     ];
-    res.render('albums', {tilte: 'Hey Albums', message: 'My Albums',images: images});
-});
+    res.render('albums', {
+      tilte: 'Hey Albums',
+      message: 'My Albums',
+      images: images,
+      layout: 'application'
+    });
+  });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
