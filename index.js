8c83a058d9245f513d2f6db28fb328c87faa87e7
1312432 <1312432@student.hcmus.edu.vn>
2017-04-10 15:21:30 +0700

    add handlebar

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var app = express();
 
 app.use(express.static('public'));
 app.use('/components', express.static('bower_components'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.engine('hbs', exphbs({}));
+app.set('view engine', 'hbs');
+
+app.get('/hbs', function (req, res) {
+   res.render('index', { title: ' Hey HomePage', message: 'Hello there!' });
+});
+
+app.get('/albums', function (req, res) {
+    var images = [
+      'image/image1.jpg',
+      'image/image2.jpg',
+    ];
+    res.render('albums', {tilte: 'Hey Albums', message: 'My Albums',images: images});
 });
 
 app.listen(3000, function () {
